3f6938b3340295236df83391889ea9ffe6211da4
akira.sato <akira.sato@mmmcorp.co.jp>
2025-10-30 22:11:57 +0900

    Add cdk.context.json for issue tracking and update EC2 stack configuration to specify region

--- bin/vscode-on-ec2-for-prototyping.ts
+++ bin/vscode-on-ec2-for-prototyping.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { VscodeOnEc2ForPrototypingStack } from '../lib/vscode-on-ec2-for-prototyping-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { VscodeOnEc2ForPrototypingStack } from "../lib/vscode-on-ec2-for-prototyping-stack";
 
 const app = new cdk.App();
-new VscodeOnEc2ForPrototypingStack(app, 'VscodeOnEc2ForPrototypingStack', {});
+new VscodeOnEc2ForPrototypingStack(app, "VscodeOnEc2ForPrototypingStack", {
+  env: {
+    region: "ap-northeast-1",
+  },
+});
